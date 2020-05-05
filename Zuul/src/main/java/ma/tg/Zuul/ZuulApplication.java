c5cfd8b51816abbf28bbab5452f4199b385023af
tghrayt <tghrayt.ysf@gmail.com>
2020-05-05 16:38:47 +0000

    commit number 2

---
+++ Zuul/src/main/java/ma/tg/Zuul/ZuulApplication.java
@@ -0,0 +1,47 @@
+package ma.tg.Zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import ma.tg.Zuul.filters.ErrorFilter;
+import ma.tg.Zuul.filters.PostFilter;
+import ma.tg.Zuul.filters.PreFilter;
+import ma.tg.Zuul.filters.RouteFilter;
+
+
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+	
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+	
+	
+	
+
+}
---
+++ Zuul/src/main/java/ma/tg/Zuul/configuration/Configuration.java
@@ -0,0 +1,7 @@
+package ma.tg.Zuul.configuration;
+
+public class Configuration {
+
+	
+	
+}
